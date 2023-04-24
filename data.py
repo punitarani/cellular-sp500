6ee5ef4562c0b185d7dbbc23491df5be797d01cd
Punit Arani <punitsai36@gmail.com>
2023-04-23 18:31:13 -0700

    Add get_and_save_sp500_stock_data()

--- data.py
+++ data.py
@@ -3,9 +3,12 @@ data.py
 """
 
 import datetime as dt
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import pandas as pd
+import pandas_datareader as web
 import pandas_datareader.data as web
+from tqdm import tqdm
 
 
 def get_sp500_tickers() -> list[str]:
@@ -45,13 +48,35 @@ def save_stock_data_as_parquet(ticker: str) -> None:
     data.to_parquet(f"data/{ticker}.parquet")
 
 
+def get_and_save_sp500_stock_data() -> None:
+    """Fetch historical stock data for S&P 500 companies and save as parquet files"""
+    tickers = get_sp500_tickers()
+    downloaded_tickers = []
+    failed_tickers = []
+
+    # Use multithreading to speed up the process
+    with ThreadPoolExecutor() as executor:
+        # Wrap the futures list with tqdm to track progress
+        futures = {executor.submit(save_stock_data_as_parquet, ticker): ticker for ticker in tickers}
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            ticker = futures[future]
+            try:
+                future.result()
+                downloaded_tickers.append(ticker)
+            except Exception as e:
+                failed_tickers.append(ticker)
+                print(f"Error downloading {ticker}: {e}")
+
+    print("\nDownloaded tickers:")
+    print(downloaded_tickers)
+
+    print("\nFailed tickers:")
+    print(failed_tickers)
+
+
 if __name__ == "__main__":
     # Get the list of S&P 500 tickers
     print("S&P500 Companies: ", get_sp500_tickers())
 
-    # Get the historical stock data for Apple
-    aapl = get_stock_data('AAPL')
-    print(aapl.head())
-
-    # Save the data to a parquet file
-    aapl.to_parquet('data/aapl.parquet')
+    # Download and save the stock data for S&P 500 companies
+    get_and_save_sp500_stock_data()
