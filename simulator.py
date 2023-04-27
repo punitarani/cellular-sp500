51001d16ad5bc30e0251c579dd12dbf739fb01e6
Punit Arani <punitsai36@gmail.com>
2023-04-27 14:07:11 -0700

    Update plot_simulation() to save to simulation.gif

--- simulator.py
+++ simulator.py
@@ -1,9 +1,12 @@
 """Cellular Automata Simulator"""
 
+import json
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import colors
+from matplotlib.animation import FuncAnimation
 from tqdm import tqdm
 
 from data import load_daily_change_df
@@ -54,8 +57,11 @@ def predict_neighbors(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarr
     return predictions
 
 
-def simulate(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarray) -> dict[str, float]:
-    # Initialize predictions dictionary with the input stock_symbol
+def simulate(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarray) -> list[dict[str, float]]:
+    # Initialize the output list
+    simulation_output = []
+
+    # Initialize the predictions dictionary with the input stock_symbol
     predictions = {stock_symbol: input_seq[-1][0]}
 
     # Initialize queue for BFS
@@ -66,7 +72,7 @@ def simulate(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarray) -> di
     updated = set()
 
     # Wrap the while loop with tqdm to show progress
-    with tqdm(total=grid.size) as pbar:
+    with tqdm(total=grid.size, desc="Simulating") as pbar:
         while queue:
             current_symbol, current_input_seq, prev_neighbor_symbol = queue.pop(0)
             if current_symbol not in visited:
@@ -96,6 +102,9 @@ def simulate(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarray) -> di
                         predictions[neighbor_stock_symbol] = prediction
                         updated.add(neighbor_stock_symbol)
 
+                    # Add the predictions to the simulation_output
+                    simulation_output.append(predictions.copy())
+
                     # If the neighbor has not been visited yet, add it to the queue
                     if neighbor_stock_symbol not in visited:
                         next_input_seq = get_trailing_stock_data(neighbor_stock_symbol, prediction)
@@ -104,44 +113,67 @@ def simulate(grid: pd.DataFrame, stock_symbol: str, input_seq: np.ndarray) -> di
                 # Update the progress bar
                 pbar.update(1)
 
-            # If the queue is empty, the current iteration is complete
-            if not queue:
-                # Reset the updated set for the next iteration
-                updated.clear()
-
-    return predictions
+    return simulation_output
 
 
-def plot_predictions(grid: pd.DataFrame, predictions: dict[str, float]):
-    # Prepare a grid to store the changed percentage values
-    percentage_grid = np.zeros_like(grid, dtype=float)
+def save_animation_with_progress_bar(animation, filename, fps, writer, total_frames):
+    with tqdm(total=total_frames, desc=f"Saving animation {filename}") as pbar:
+        def progress_callback(current_frame, total_frames):
+            pbar.update(1)
 
-    # Fill the grid with the corresponding percentage changes
-    for symbol, change in predictions.items():
-        row, col = find_stock_position(grid, symbol)
-        percentage_grid[row, col] = change
+        animation.save(filename, fps=fps, writer=writer, progress_callback=progress_callback)
+
+
+def plot_simulation(grid: pd.DataFrame, simulations: list[dict[str, float]]):
+    fig, ax = plt.subplots()
 
     # Create a colormap with a variant of green for positive values and a variant of red for negative values
     cmap = colors.LinearSegmentedColormap.from_list("stock_changes", ["red", "white", "green"])
 
-    # Plot the grid with the percentage changes
-    plt.imshow(percentage_grid, cmap=cmap, vmin=-1, vmax=1)
-    plt.colorbar()
-    plt.title("Stock Change Percentages")
+    # Prepare a grid to store the changed percentage values
+    percentage_grid = np.zeros_like(grid, dtype=float)
+
+    def update(frame):
+        ax.clear()
+        simulation = simulations[frame]
+
+        # Fill the grid with the corresponding percentage changes
+        for symbol, change in simulation.items():
+            row, col = find_stock_position(grid, symbol)
+            percentage_grid[row, col] = change
+
+        # Plot the grid with the percentage changes
+        ax.imshow(percentage_grid, cmap=cmap, vmin=-1, vmax=1)
+        ax.set_title(f"Iteration {frame + 1}")
+
+    # Calculate the interval and fps so animation is ~10s long
+    num_frames = len(simulations)
+    interval = 10000 // num_frames
+    fps = (1000 // interval) * 10
+
+    # Create the animation
+    ani = FuncAnimation(fig, update, frames=num_frames, interval=interval, repeat=False)
+
+    # Save the animation as a video file
+    save_animation_with_progress_bar(ani, 'simulation.gif', fps=fps, writer='pillow', total_frames=num_frames)
+    print("Animation saved to simulation.gif")
+
     plt.show()
 
 
 if __name__ == "__main__":
     # Input data
     ticker = "AAPL"
-    change = 2
+    change = 0.5825
 
     # Get the input sequence
     input_seq = get_trailing_stock_data(ticker, change)
 
     # Simulate all the cells in the grid
-    predictions = simulate(grid, ticker, input_seq)
-    print(predictions)
+    simulations = simulate(grid, ticker, input_seq)
+    # Save the simulations to a file
+    with open("simulations.json", "w") as f:
+        json.dump(simulations, f)
 
-    # Plot the predictions
-    plot_predictions(grid, predictions)
+    # Plot the animation of the simulations
+    plot_simulation(grid, simulations)
